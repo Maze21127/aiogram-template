7205b8253d0efa7bfc12346f1348500e50244474
maze <invoker322sf@gmail.com>
2024-01-03 19:20:23 +1000

    refactor: добавлены type hints

--- {{project_name}}/src/bot/main.py
+++ {{project_name}}/src/bot/main.py
@@ -8,8 +8,8 @@ from src.bot.handlers.commands import router as commands_router
 from src.bot.loader import bot, dp
 
 
-async def set_default_commands(bot: Bot):
-    await bot.set_my_commands(
+async def set_default_commands(bot_: Bot) -> None:
+    await bot_.set_my_commands(
         [
             BotCommand(command="start", description="Запустить бота"),
             BotCommand(command="help", description="Получить помощь"),
@@ -18,16 +18,16 @@ async def set_default_commands(bot: Bot):
     logger.info("Set default commands")
 
 
-async def on_startup(bot: Bot):
-    await set_default_commands(bot)
+async def on_startup(bot_: Bot) -> None:
+    await set_default_commands(bot_)
     logger.info("Bot started")
 
 
-async def on_shutdown(bot: Bot):
+async def on_shutdown(bot_: Bot) -> None:
     logger.info("Bot shutdown")
 
 
-async def main():
+async def main() -> None:
     dp.startup.register(on_startup)
     dp.shutdown.register(on_shutdown)
 
