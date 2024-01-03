793a0c9da9cef0bdc6239854aef18beb5c6872ba
maze <invoker322sf@gmail.com>
2024-01-03 19:52:06 +1000

    refactor: изменено название переменной

--- {{project_name}}/src/bot/main.py
+++ {{project_name}}/src/bot/main.py
@@ -5,11 +5,12 @@ from aiogram.types import BotCommand
 from loguru import logger
 
 from src.bot.handlers.commands import router as commands_router
-from src.bot.loader import bot, dp
+from src.bot.loader import bot as bot_instance
+from src.bot.loader import dp
 
 
-async def set_default_commands(bot_: Bot) -> None:
-    await bot_.set_my_commands(
+async def set_default_commands(bot: Bot) -> None:
+    await bot.set_my_commands(
         [
             BotCommand(command="start", description="Запустить бота"),
             BotCommand(command="help", description="Получить помощь"),
@@ -18,12 +19,12 @@ async def set_default_commands(bot_: Bot) -> None:
     logger.info("Set default commands")
 
 
-async def on_startup(bot_: Bot) -> None:
-    await set_default_commands(bot_)
+async def on_startup(bot: Bot) -> None:
+    await set_default_commands(bot)
     logger.info("Bot started")
 
 
-async def on_shutdown(bot_: Bot) -> None:
+async def on_shutdown(bot: Bot) -> None:
     logger.info("Bot shutdown")
 
 
@@ -33,9 +34,9 @@ async def main() -> None:
 
     dp.include_router(commands_router)
 
-    await bot.delete_webhook(drop_pending_updates=True)
+    await bot_instance.delete_webhook(drop_pending_updates=True)
 
-    await dp.start_polling(bot)
+    await dp.start_polling(bot_instance)
 
 
 if __name__ == "__main__":
